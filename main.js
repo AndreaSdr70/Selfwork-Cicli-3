3d9d199765e06be3f7e2c5c704c00bf1290b75a8
AndreaSdr70 <andrea@sdringola.it>
2024-07-14 17:42:17 +0200

    Selfwork Cicli 3

---
+++ main.js
@@ -0,0 +1,14 @@
+let scelta = prompt(`Scegli una bevanda: \n 1-Acqua \n  2-Coca Cola \n  3-Birra`);
+
+if(scelta == 1){
+    console.log(`Ecco la tua Acqua`);    
+}else if (scelta == 2){
+    console.log(`Ecco quì la tua Coca Cola`);
+}else if (scelta == 3){
+    console.log(`Ecco quì la tua Birra`);
+}else{ 
+    console.log(`Scelta non valida`);
+}
+
+
+
